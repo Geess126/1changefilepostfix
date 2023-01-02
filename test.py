b3ab580a7333f7f590c2b65d5c352fa8039ef156
guoshan <samguo_321@126.com>
2023-01-02 22:19:31 +0800

    step 1

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/1/2 22:17
+# @Author  : Gs
+# @Site    : 
+# @File    : test.py
+# @Software: PyCharm
+
+
+import numpy as np
+
+print("step 1")
